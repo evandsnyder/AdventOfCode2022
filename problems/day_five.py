0f73f53c4ed9ce52bc9a9b42db4f0492d0ca8ce4
Evan Snyder <evansnyder16@gmail.com>
2022-12-06 12:12:10 -0500

    Added days 5 and 6

--- problems/day_five.py
+++ problems/day_five.py
@@ -2,17 +2,98 @@ from problems.problem import Problem
 
 
 class DayFive(Problem):
+    file_data: list[str] = []
+    towers: list[list] = []
+    instructions: list[str] = []
+
     def __init__(self, filename, debug):
         super().__init__(filename, debug)
 
-    def read_input(self):
+    def read_input(self)-> list[str]:
         with open(self.filename, 'r') as f:
-            data = f.readlines()
+            self.file_data = f.read().splitlines()
 
-        return data
+        return self.file_data
 
     def part_one(self):
-        print(f'Part One: {0}')
+        self.read_input()
+        self.create_stacks()
+
+        for inst in self.instructions:
+            parts = inst.split()
+            count = int(parts[1])
+            start = int(parts[3]) - 1
+            end = int(parts[5]) - 1
+
+            while len(self.towers[start]) > 0 and count > 0:
+                self.towers[end].append(self.towers[start].pop())
+                count -= 1
+
+            self.debug(f'Moving {count} from {start} to {end}')
+        
+
+        print(f'Part One: {self.get_solution()}')
 
     def part_two(self):
-        print(f'Part Two: {0}')
+        # maintain the same order now when moving multiple crates...
+
+        # Rebuilding for a fresh start...
+        self.create_stacks()
+        for inst in self.instructions:
+            parts = inst.split()
+            count = int(parts[1])
+            start = int(parts[3]) - 1
+            end = int(parts[5]) - 1
+
+            move_stack: list[str] = []
+            while len(self.towers[start]) > 0 and count > 0:
+                move_stack.append(self.towers[start].pop())
+                count -= 1
+            
+            while len(move_stack) > 0:
+                self.towers[end].append(move_stack.pop())
+
+        print(f'Part Two: {self.get_solution()}')
+
+    def create_stacks(self):
+        tower_entries: list[str] = []
+        # iterate until empty line....
+        i = 0
+        while self.file_data[i] != '':
+            self.debug(f'{i}')
+            tower_entries.append(self.file_data[i])
+            i += 1
+        
+        i += 1
+
+        tower_count = int(tower_entries[-1].rstrip()[-1])
+        tower_entries = tower_entries[:-1]
+        self.debug(self.file_data[i])
+        self.debug(f'Tower count: {tower_count}')
+
+        self.instructions = self.file_data[i:]
+
+        self.towers: list[list] = [] 
+
+        # Create each tower that we need...
+        for i in range(tower_count):
+            self.towers.append([])
+
+        # iterate in reverse through all possible towers and add them to the relevant stack...
+        for row in reversed(tower_entries):
+            row = row.rstrip()
+            for j in range(0, len(row), 4):
+                if row[j + 1] != ' ':
+                    self.towers[j // 4].append(row[j+1])
+                    self.debug(f'{row[j+1]}: {j}')
+        self.print_towers()
+        
+    def print_towers(self):
+        for tower in self.towers:
+            self.debug(''.join(tower))
+        
+    def get_solution(self) -> str:
+        sol = ''
+        for tower in self.towers:
+            sol += tower[-1]
+        return sol
--- problems/day_six.py
+++ problems/day_six.py
@@ -7,12 +7,51 @@ class DaySix(Problem):
 
     def read_input(self):
         with open(self.filename, 'r') as f:
-            data = f.readlines()
+            data = f.read().strip()
 
         return data
 
     def part_one(self):
-        print(f'Part One: {0}')
+        if self.DEBUG:
+            self.run_test_cases(4)
+            return
+
+        start_of_packet: int = self.find_start_of_sequence(self.read_input(), 4)
+        print(f'Part One: {start_of_packet}')
 
     def part_two(self):
-        print(f'Part Two: {0}')
+        if self.DEBUG:
+            self.run_test_cases(14)
+            return
+
+        start_of_message = self.find_start_of_sequence(self.read_input(), 14)
+        print(f'Part Two: {start_of_message}')
+    
+    def is_unique(self, candidate: str) -> bool:
+        known_values: set = set()
+
+        for char in candidate:
+            if ord(char) in known_values: return False
+            known_values.add(ord(char))
+
+        return True
+    
+    
+    def find_start_of_sequence(self, buffer: str, sequence_size: int) -> int:
+        i: int = 0
+        while i < len(buffer) - sequence_size:
+            potential: str = buffer[i:i+sequence_size]
+            self.debug(f'Potential sequence: {potential}')
+            if self.is_unique(potential):
+                break
+            i += 1
+        return i + sequence_size
+    
+    def run_test_cases(self, sequence_size) -> None:
+        test_cases = ['bvwbjplbgvbhsrlpgdmjqwftvncz', 'nppdvjthqldpwncqszvftbrmjlhg', 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'mjqjpqmgbljsphdztnvjfqwrcgsmlb']
+        test_solutions = [5,6,10,11,7]
+        if sequence_size == 14:
+            test_solutions = [23, 23, 29, 26, 19]
+
+        for i in range(len(test_cases)):
+            self.debug(f'Test case {i+1} {"passed" if self.find_start_of_sequence(test_cases[i], sequence_size) == test_solutions[i] else "failed"}!')
