852d67e611af6ab00a244e70ed1a271e573ae94a
Evan Snyder <evansnyder16@gmail.com>
2022-12-23 21:34:38 -0500

    Added days 12 and 13

--- problems/day_thirteen.py
+++ problems/day_thirteen.py
@@ -1,18 +1,130 @@
 from problems.problem import Problem
+import functools
 
 
 class DayThirteen(Problem):
     def __init__(self, filename, debug):
         super().__init__(filename, debug)
+        self.read_input()
 
     def read_input(self):
         with open(self.filename, 'r') as f:
-            data = f.readlines()
-
-        return data
+            data = f.read().split('\n\n')
+        
+        self.packets = []
+        for set_data in data:
+            self.packets.append(set_data.split('\n'))
 
     def part_one(self):
-        print(f'Part One: {0}')
+        result = 0
+        for i in range(len(self.packets)):
+            self.debug(f'== Pair {i+1} ==')
+            if self.is_in_order(self.packets[i]) > 0:
+                result += i+1
+        print(f'Part One: {result}')
 
     def part_two(self):
-        print(f'Part Two: {0}')
+        # build a list of parsed packets and add dividers...
+        all_packets = []
+        for packet in self.packets:
+            all_packets.append(self.build_list(packet[0][1:-1]))
+            all_packets.append(self.build_list(packet[1][1:-1]))
+        
+        all_packets.append([[2]])
+        all_packets.append([[6]])
+
+        # Using the compare function defined below, sort the packets...
+        all_packets = sorted(all_packets, key=functools.cmp_to_key(lambda a, b: self.compare(a,b)), reverse=True)
+
+        # find index of [[2]] and [[6]]
+        two = 0
+        six = 0
+        for i in range(len(all_packets)):
+            print(all_packets[i])
+            if all_packets[i] == [[2]]:
+                two = i+1
+            if all_packets[i] == [[6]]:
+                six = i+1
+        
+        self.debug(f"TWO: {two}\tSIX: {six}")
+        print(f'Part Two: {two*six}')
+    
+    def is_in_order(self, packet_set: list[str]) -> int:
+        left = self.build_list(packet_set[0][1:-1])
+        right = self.build_list(packet_set[1][1:-1])
+
+        return self.compare(left, right)
+    
+    def compare(self, left, right, depth = 0) -> int:
+        self.debug(f'{"  "*depth}- Compare {left} vs {right}')
+
+        if isinstance(left, int) and isinstance(right, int):
+            if left == right:
+                return 0
+            r = left < right
+            if r:
+                self.debug(f'{" "*(depth+2)}- Left side is smaller, so inputs are in the right order')
+                return 1
+            else:
+                self.debug(f'{" "*(depth+2)}- Left side is larger, so inputs are NOT in the right order')
+                return -1
+        
+        if isinstance(left, list) and isinstance(right, list):
+            # iterate through each??
+            i = 0
+            left_len = len(left)
+            right_len = len(right)
+            t = 0
+            while t == 0 and i < left_len and i < right_len:
+                t = self.compare(left[i], right[i], depth + 1)
+                i += 1
+
+            # if left ran out first, we're good
+            if t != 0:
+                return t
+
+            if left_len == right_len:
+                # Inconclusive???
+                return 0
+            
+            if i == left_len:
+                self.debug(f'{" "*(depth+3)}- Left side ran out of items, so inputs are in the right order')
+                return 1
+            if i == right_len:
+                self.debug(f'{" "*(depth+3)}- Right side ran out of items, so inputs are NOT in the right order')
+                return -1
+            return t
+
+        
+        if isinstance(left, int):
+            left = [left]
+        else:
+            right = [right]
+        
+        return self.compare(left, right, depth + 1)
+    
+    def build_list(self, raw_packet: str)-> list[list|int]:
+        # if first token is not a '[', we can seek till a ','
+        # else we need to find the end of the list...
+        result = []
+        while len(raw_packet):
+            if raw_packet[0] == '[':
+                seeker = 1
+                depth = 1
+                while depth > 0 and seeker < len(raw_packet):
+                    if raw_packet[seeker] == ']':
+                        depth -= 1
+                    elif raw_packet[seeker] == '[':
+                        depth += 1
+                    seeker += 1
+                result.append(self.build_list(raw_packet[1:seeker-1]))
+                raw_packet = raw_packet[seeker+1:]
+            else:
+                try:
+                    i = raw_packet.index(',')
+                    result.append(int(raw_packet[:i]))
+                    raw_packet = raw_packet[i+1:]
+                except ValueError:
+                    result.append(int(raw_packet))
+                    raw_packet = ''
+        return result
--- problems/day_twelve.py
+++ problems/day_twelve.py
@@ -1,18 +1,159 @@
 from problems.problem import Problem
+import heapq
+
+"""
+This is extremely slow for part two... need to figure out how to make it faster...
+"""
 
 
+class Node():
+    def __init__(self, char: str, x: int, y: int):
+        if char == 'E':
+            self.value = 25
+        elif char == 'S':
+            self.value = 0
+        else:
+            self.value = ord(char) - 97
+
+        self.x = x
+        self.y = y
+        self.char = char
+        self.visited = False
+        self.neighbors = []
+        self.distance = 100_000
+    
+    def print_neighbors(self):
+        print(self.neighbors)
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self) -> str:
+        return f'Node({self.char}, {self.value}, {self.distance}) [{self.x}][{self.y}]'
 class DayTwelve(Problem):
+    VISITED = 100_000
+
     def __init__(self, filename, debug):
         super().__init__(filename, debug)
+        self.start = Node('a',0,0)
+        self.end = Node('z',0,0)
+        self.read_input()
 
     def read_input(self):
         with open(self.filename, 'r') as f:
-            data = f.readlines()
+            data = f.read().splitlines()
+        
+        self.graph = []
+        for x in range(len(data)):
+            t = []
+            for y in range(len(data[0])):
+                t.append(Node(data[x][y], x, y))
+            self.graph.append(t)
+                
 
-        return data
 
+        for i in range(len(self.graph)):
+            for j in range(len(self.graph[0])):
+                if self.graph[i][j].char == 'S':
+                    self.start = self.graph[i][j]
+                    self.start.value = ord('a')- 97
+                if self.graph[i][j].char == 'E':
+                    self.end = self.graph[i][j]
+                    self.end.value = ord('z')-97
+
+                neighbor_coords = [
+                    (i - 1, j),
+                    (i + 1, j),
+                    (i, j - 1),
+                    (i, j + 1)
+                ]
+
+                for edge in neighbor_coords:
+                    if edge[0] >= 0 and edge[0] < len(self.graph) and edge[1] >= 0 and edge[1] < len(self.graph[0]) and self.can_step(self.graph[i][j], self.graph[edge[0]][edge[1]]):
+                        self.graph[i][j].neighbors.append(self.graph[edge[0]][edge[1]])
+        
+        self.flat_graph = []
+        for row in self.graph:
+            for c in row:
+                self.flat_graph.append(c)
+            
+ 
     def part_one(self):
-        print(f'Part One: {0}')
+        # Find start node.. where char is s
+        start_index: int = 0
+        end_index: int = 0
+
+        for i in range(len(self.flat_graph)):
+            if self.flat_graph[i].char == 'S':
+                start_index = i
+            if self.flat_graph[i].char == 'E':
+                end_index = i
+        self.dijkstra_shortest_path(start_index)
+        print(f'Part One: {self.flat_graph[end_index].distance}')
 
     def part_two(self):
-        print(f'Part Two: {0}')
+        # Find all potential starting points...
+        # for node in self.flat_graph
+        # Can I find the indexes of all start positions?
+        indexes = []
+
+        end_index = 0
+        for i in range(len(self.flat_graph)):
+            if self.flat_graph[i].value == 0:
+                indexes.append(i)
+            if self.flat_graph[i].char == 'E':
+                end_index = i
+
+        self.debug(f'Potential starting positions: {len(indexes)}')
+        lengths = []
+        for index in indexes:
+            # print(self.flat_graph[index])
+
+            self.dijkstra_shortest_path(index)
+            lengths.append(self.flat_graph[end_index].distance)
+        
+        print(lengths)
+        print(f'Part Two: {min(lengths)}')
+
+    def dijkstra_shortest_path(self,start_index: int):
+        # self.debug(f'Start node neighbors: {self.start.neighbors}')
+        # self.debug(f'End node neighbors: {self.end.neighbors}')
+
+        for node in self.flat_graph:
+            node.visited = False
+            node.distance = 100_000
+        
+        self.flat_graph[start_index].distance = 0
+
+        # self.print_graph(self.flat_graph)
+        queue = [(v.distance, v) for v in self.flat_graph]
+        heapq.heapify(queue)
+
+        while len(queue):
+            node = heapq.heappop(queue)[1]
+            node.visited = True
+
+            new_dist = node.distance + 1
+            for neighbor in node.neighbors:
+                #self.debug(f'Testing neighbor: {neighbor}')
+
+                if new_dist < neighbor.distance:
+                    #self.debug(f'We\'ve got a new shorter distance: {new_dist}')
+                    neighbor.distance = new_dist
+            
+            queue = [(v.distance, v) for v in self.flat_graph if not v.visited]
+            heapq.heapify(queue)
+    
+    def can_step(self, a: Node, b: Node):
+       # self.debug(f'Comparing {b} <= {a} + 1')
+        return b.value <= a.value+1
+
+    
+    def print_graph(self, graph):
+        for node in graph:
+            print(str(node))
+            node.print_neighbors()
+            
